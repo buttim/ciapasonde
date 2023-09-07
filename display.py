5a8f4c59041b9fbf7dfbfb2ea50e7b7b68ce8144
buttim <maurizio.butti@gmail.com>
2023-09-07 19:16:48 +0200

    adds support for LCD HAT

--- display.py
+++ display.py
@@ -6,6 +6,27 @@ from PIL import ImageDraw
 from PIL import ImageFont
 import RPi.GPIO as GPIO
 
+HAT=1
+if HAT:
+	RST=27
+	DC=25
+	BACKLIGHT=24
+	ROTATION=90
+	WIDTH=128
+	HEIGHT=128
+	BTN1=21
+	BTN2=20
+	BTN3=16
+else:
+	RST=24
+	DC=25
+	BACKLIGHT=23
+	ROTATION=270
+	WIDTH=128
+	HEIGHT=160
+	BTN1=5
+	BTN2=6
+	BTN3=6
 
 button1=False
 button2=False
@@ -31,19 +52,19 @@ class Display:
     self.disp=ST7735.ST7735(
       port=0,
       cs=0,
-      rst=24,
-      dc=25,
-      backlight=23,
-      rotation=270,
+      rst=RST,
+      dc=DC,
+      backlight=BACKLIGHT,
+      rotation=ROTATION,
       spi_speed_hz=4000000,
       invert=False,
       offset_left=0,
       offset_top=0,
-      width=128,
-      height=160
+      width=WIDTH,
+      height=WIDTH
     )
     self.disp.begin()
-    self.img = Image.new('RGB', (self.disp.width,self.disp.height), color=(0, 255, 0))
+    self.img = Image.new('RGB', (self.disp.width,self.disp.height), color=(255, 0, 0))
 
     self.draw = ImageDraw.Draw(self.img)
 
@@ -51,14 +72,14 @@ class Display:
     self.fontSmall = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 10)
 
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(BTN1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(BTN2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     button1=False
     button2=False
 
-    GPIO.add_event_detect(5, GPIO.FALLING, callback=onButton1, bouncetime=200)
-    GPIO.add_event_detect(6, GPIO.FALLING, callback=onButton2, bouncetime=200)
+    GPIO.add_event_detect(BTN1, GPIO.FALLING, callback=onButton1, bouncetime=200)
+    GPIO.add_event_detect(BTN2, GPIO.FALLING, callback=onButton2, bouncetime=200)
 
     self.ip='?.?.?.?'
     self.id='????????'
@@ -74,13 +95,13 @@ class Display:
   def update(self):
     if self.asking: return
     self.lock.acquire()
-    self.draw.rectangle((0, 0, self.disp.width, self.disp.height), fill=(0, 0, 255))
+    self.draw.rectangle((0, 0, self.disp.width, self.disp.height), fill=(255, 0, 0))
     self.draw.text((5,0),f"{self.id}",font=self.font,fill=(0,0,0))
     self.draw.text((5,20),f"{self.type}@{self.freq}",font=self.font,fill=(0,0,0))
     self.draw.text((5,40),f"{self.lat}",font=self.font,fill=(0,0,0))
     self.draw.text((5,55),f"{self.lng}",font=self.font,fill=(0,0,0))
     self.draw.text((5,75),f"{self.alt}m",font=self.font,fill=(0,0,0))
-    self.draw.text((5,115),f"IP {self.ip}     {'(BT)' if self.connected else ''}",font=self.fontSmall,fill=(0,0,0))
+    self.draw.text((5,115),f"IP {self.ip}  {'(BT)' if self.connected else ''}",font=self.fontSmall,fill=(0,0,0))
     try:
       self.disp.display(self.img)
     except:
